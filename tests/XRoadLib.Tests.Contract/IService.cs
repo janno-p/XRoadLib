8bb21b385c019d5415333d2a1ab86f113b51998b
Janno Põldma <janno.poldma@gmail.com>
2016-01-20 18:06:00 +0200

    Void type needs separate handling.

--- tests/XRoadLib.Tests.Contract/IService.cs
+++ tests/XRoadLib.Tests.Contract/IService.cs
@@ -5,7 +5,7 @@ namespace XRoadLib.Tests.Contract
     public interface IService
     {
         [XRoadService("Service1")]
-        void Service1(
+        int Service1(
             [XRoadParameter(IsOptional = true)] ParamType1 param1,
             [XRoadParameter(IsOptional = true)] ParamType2 param2,
             [XRoadParameter(IsOptional = true)] ParamType3 param3
