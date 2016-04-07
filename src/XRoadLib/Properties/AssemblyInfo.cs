2b8a00c172bc42b55d7c20814e133109af636793
Janno Põldma <janno.poldma@gmail.com>
2016-04-07 16:18:35 +0300

    Bump version to 1.0.0-beta018

--- src/XRoadLib/Properties/AssemblyInfo.cs
+++ src/XRoadLib/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("A .NET library for implementing service interfaces of X-Road providers using Code-First Development approach.")]
 [assembly: AssemblyVersionAttribute("1.0.0")]
 [assembly: AssemblyFileVersionAttribute("1.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0-beta017")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0-beta018")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
