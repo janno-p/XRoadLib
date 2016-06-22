da7a0c4f7e0eb5f8235e032437dec5fdd9e2615f
Janno Põldma <janno.poldma@gmail.com>
2016-06-22 18:29:58 +0300

    Converted some tests to C#/Xunit.

---
+++ test/XRoadLib.Tests/Globals.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Xml.Linq;
+using XRoadLib.Protocols;
+using XRoadLib.Schema;
+using XRoadLib.Tests.Contract;
+using XRoadLib.Tests.Contract.Configuration;
+
+namespace XRoadLib.Tests
+{
+    public static class Globals
+    {
+        public static XRoadProtocol XRoadProtocol20 { get; } = CustomXRoad20Protocol.Instance;
+        public static XRoadProtocol XRoadProtocol31 { get; } = CustomXRoad31Protocol.Instance;
+        public static XRoadProtocol XRoadProtocol40 { get; }
+
+        static Globals()
+        {
+            var protocol = new XRoad40Protocol("http://test-producer.x-road.eu/");
+            protocol.SetContractAssembly(typeof(Class1).GetTypeInfo().Assembly, null, 1u, 2u);
+            XRoadProtocol40 = protocol;
+        }
+
+        public static IContentDefinition GetTestDefinition(Type type)
+        {
+            return new TestDefinition(type);
+        }
+
+        private class TestDefinition : IContentDefinition
+        {
+            public bool UseXop { get; }
+            public Type RuntimeType { get; }
+            public XName TypeName { get; }
+            public ArrayItemDefinition ArrayItemDefinition { get; }
+            public bool MergeContent { get; }
+
+            public TestDefinition(Type type)
+            {
+                RuntimeType = type;
+            }
+        }
+    }
+}
---
+++ test/XRoadLib.Tests/Serialization/XRoadMessageTest.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using XRoadLib.Protocols;
+using XRoadLib.Serialization;
+using Xunit;
+
+namespace XRoadLib.Tests.Serialization
+{
+    public class XRoadMessageTest
+    {
+        private readonly XRoadProtocol[] supportedProtocols = new XRoadProtocol[] { Globals.XRoadProtocol20, Globals.XRoadProtocol31, Globals.XRoadProtocol40 };
+
+        [Fact]
+        public void CanWriteEmptyContent()
+        {
+            var contentLength = WriteContent();
+            Assert.Equal(0L, contentLength);
+        }
+
+        [Fact]
+        public void CanWriteContentWithoutAttachments()
+        {
+            var contentLength = WriteContent("test");
+            Assert.Equal(4L, contentLength);
+        }
+
+        [Fact]
+        public void CanWriteContentWithAttachments()
+        {
+            using (var x = new XRoadAttachment(new MemoryStream(new byte[] { 1, 2, 3, 4 })))
+            using (var y = new XRoadAttachment(new MemoryStream(new byte[] { 5, 6, 7, 8, 9 })))
+            {
+                var contentLength = WriteContent("test2", new[] { x, y });
+                Assert.Equal(534L, contentLength);
+            }
+        }
+
+        [Fact]
+        public void CanReadEmptyContentWithoutAttachments()
+        {
+            using (var stream = new MemoryStream())
+            using (var reader = new XRoadMessageReader(stream, new WebHeaderCollection(), Encoding.UTF8, Path.GetTempPath(), supportedProtocols))
+            using (var message = new XRoadMessage())
+            {
+                reader.Read(message, false);
+                Assert.Equal(0L, message.ContentLength);
+            }
+        }
+
+        [Fact]
+        public void CanReadContentWithoutAttachments()
+        {
+            using (var stream = new MemoryStream())
+            using (var streamWriter = new StreamWriter(stream, Encoding.UTF8))
+            {
+                streamWriter.Write("<Envelope xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:id=\"http://x-road.eu/xsd/identifiers\" xmlns:repr=\"http://x-road.eu/xsd/representation.xsd\">\r\n");
+                streamWriter.Write("  <Header xmlns:xrd=\"http://x-road.ee/xsd/x-road.xsd\">\r\n");
+                streamWriter.Write("  </Header>\r\n");
+                streamWriter.Write("  <Body />\r\n");
+                streamWriter.Write("</Envelope>");
+                streamWriter.Flush();
+
+                stream.Position = 0L;
+                using (var reader = new XRoadMessageReader(stream, new WebHeaderCollection(), Encoding.UTF8, Path.GetTempPath(), supportedProtocols))
+                using (var message = new XRoadMessage())
+                {
+                    reader.Read(message, false);
+                    Assert.Equal(254L, message.ContentLength);
+                }
+            }
+        }
+
+        [Fact]
+        public void CanReadContentWithAttachments()
+        {
+            using (var stream = new MemoryStream())
+            using (var streamWriter = new StreamWriter(stream, Encoding.UTF8))
+            {
+                streamWriter.Write("\r\n");
+                streamWriter.Write("--5e7a8827-5850-45be-9a1e-8bbf6aff5da7\r\n");
+                streamWriter.Write("Content-Type: text/xml; charset=UTF-8\r\n");
+                streamWriter.Write("Content-Transfer-Encoding: 8bit\r\n");
+                streamWriter.Write("Content-ID: <rQI0gpIFuQMxlrqBj3qHKw==>\r\n");
+                streamWriter.Write("\r\n");
+                streamWriter.Write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n");
+                streamWriter.Write("<Envelope xmlns=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:id=\"http://x-road.eu/xsd/identifiers\" xmlns:repr=\"http://x-road.eu/xsd/representation.xsd\">\r\n");
+                streamWriter.Write("  <Header xmlns:xrd=\"http://x-road.ee/xsd/x-road.xsd\">\r\n");
+                streamWriter.Write("  </Header>\r\n");
+                streamWriter.Write("  <Body />\r\n");
+                streamWriter.Write("</Envelope>\r\n");
+                streamWriter.Write("\r\n");
+                streamWriter.Write("--5e7a8827-5850-45be-9a1e-8bbf6aff5da7\r\n");
+                streamWriter.Write("Content-Type: application/octet-stream\r\n");
+                streamWriter.Write("Content-Transfer-Encoding: binary\r\n");
+                streamWriter.Write("Content-ID: <CNbAWiFRKnmh3+udKo8mLw==>\r\n");
+                streamWriter.Write("\r\n");
+                streamWriter.Write("proovikas\r\n");
+                streamWriter.Write("--5e7a8827-5850-45be-9a1e-8bbf6aff5da7\r\n");
+                streamWriter.Write("Content-Type: application/octet-stream\r\n");
+                streamWriter.Write("Content-Transfer-Encoding: binary\r\n");
+                streamWriter.Write("Content-ID: <qrOlKraewrdRAu86cFnqwg==>\r\n");
+                streamWriter.Write("\r\n");
+                streamWriter.Write("testikas sisu\r\n");
+                streamWriter.Write("--5e7a8827-5850-45be-9a1e-8bbf6aff5da7--");
+                streamWriter.Flush();
+
+
+                var headers = new WebHeaderCollection();
+                headers["Content-Type"] = "multipart/related; type=\"application/xml+xop\"; start=\"rQI0gpIFuQMxlrqBj3qHKw==\"; boundary=\"5e7a8827-5850-45be-9a1e-8bbf6aff5da7\"";
+
+                stream.Position = 0L;
+                using (var reader = new XRoadMessageReader(stream, headers, Encoding.UTF8, Path.GetTempPath(), supportedProtocols))
+                using (var message = new XRoadMessage())
+                {
+                    reader.Read(message, false);
+                    Assert.Equal(838L, message.ContentLength);
+                }
+            }
+        }
+
+        private static long WriteContent(string content = null, IEnumerable<XRoadAttachment> attachments = null)
+        {
+            using (var outputStream = new MemoryStream())
+            using (var contentStream = new MemoryStream())
+            {
+                if (content != null)
+                {
+                    var buffer = Encoding.UTF8.GetBytes(content);
+                    contentStream.Write(buffer, 0, buffer.Length);
+                }
+
+                using (var message = new XRoadMessage(contentStream))
+                {
+                    (attachments ?? Enumerable.Empty<XRoadAttachment>()).ToList().ForEach(message.AllAttachments.Add);
+                    message.SaveTo(outputStream, x => {}, (k, v) => {});
+
+                    return message.ContentLength;
+                }
+            }
+        }
+    }
+}
