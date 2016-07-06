d15bbace8d1a858548e41922cfc8254047e5b6b3
Janno Põldma <janno.poldma@gmail.com>
2016-07-06 14:23:05 +0300

    Try to fix travis.

--- build.fsx
+++ build.fsx
@@ -158,11 +158,40 @@ Target "Build" (fun _ ->
 // Run the unit tests using test runner
 
 Target "RunTests" (fun _ ->
-    !! testAssemblies
-    |> NUnit3 (fun p ->
-        { p with
-            TimeOut = TimeSpan.FromMinutes 20.
-            ResultSpecs = ["TestResults.xml"] })
+    let framework =
+        match getBuildParamOrDefault "Framework" "coreclr" with
+        | "coreclr" -> "netcoreapp1.0"
+        | "netfx" -> "net451"
+        | fw -> failwithf "Unexpected framework value `%s`" fw
+    let linuxVersion =
+        if framework = "net451" then
+            let result = ExecProcessAndReturnMessages (fun p ->
+                            p.FileName <- "dotnet"
+                            p.Arguments <- "--info"
+                            ) (TimeSpan.FromMinutes 1.0)
+            let values =
+                result.Messages
+                |> Seq.choose (fun x -> match x.Split([| ':' |], 2) |> Array.map (trim) with [| k; v |] -> Some(k, v) | _ -> None)
+                |> dict
+            match values.TryGetValue("OS Platform"), values.TryGetValue("RID") with
+            | (true, "Linux"), (true, os) -> Some(os)
+            | _ -> None
+        else None
+    match linuxVersion with
+    | Some(os) ->
+        let path = sprintf "test/XRoadLib.Tests/bin/Debug/%s" framework
+        ExecProcess (fun p ->
+            p.FileName <- "mono"
+            p.Arguments <- "dotnet-test-xunit.exe XRoadLib.Tests.dll"
+            p.WorkingDirectory <- sprintf "test/XRoadLib.Tests/bin/Debug/%s/%s" framework os
+            ) (TimeSpan.FromMinutes 10.0)
+        |> ignore
+    | None ->
+        ExecProcess (fun p ->
+            p.FileName <- "dotnet"
+            p.Arguments <- sprintf "test test/XRoadLib.Tests --framework %s" framework
+            ) (TimeSpan.FromMinutes 10.0)
+        |> ignore
 )
 
 #if MONO
@@ -393,7 +422,7 @@ Target "All" DoNothing
 "Clean"
   ==> "AssemblyInfo"
   ==> "BuildDebug"
-  ==> "RunTests"
+//  ==> "RunTests"
   ==> "Build"
   ==> "CopyBinaries"
   ==> "GenerateReferenceDocs"
