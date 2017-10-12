e618fb023b5942b3351c467be8894f2ca62ebeec
Janno Põldma <janno.poldma@gmail.com>
2017-10-12 16:08:29 +0300

    Appveyor fix.

--- samples/Calculator/Contract/ISumOfIntegers.cs
+++ samples/Calculator/Contract/ISumOfIntegers.cs
@@ -5,6 +5,8 @@ namespace Calculator.Contract
     public interface ISumOfIntegers
     {
         [XRoadService("SumOfIntegers")]
+        [XRoadTitle("en", "Sum of integers")]
+        [XRoadNotes("en", "Calculates sum of two user provided integers and returns the result.")]
         int Sum(AddRequest request);
     }
 }
