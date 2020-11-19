2f8f85a5c2672e625db329485129e5abed16c867
Janno Põldma <janno.poldma@gmail.com>
2020-11-19 13:08:07 +0200

    Add unit test

--- test/XRoadLib.Tests/Serialization/XRoadAttachmentTest.cs
+++ test/XRoadLib.Tests/Serialization/XRoadAttachmentTest.cs
@@ -82,5 +82,38 @@ namespace XRoadLib.Tests.Serialization
 
             Assert.Equal("\r\n", output);
         }
+
+        [Fact]
+        public async Task CanWriteMultilineBase64Again()
+        {
+            const string text = @"Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
+
+            using var outputStream = new MemoryStream();
+            using var writer = new XRoadMessageWriter(outputStream);
+
+            using (var contentStream = new MemoryStream(Encoding.UTF8.GetBytes(text)))
+            using (var attachment = new XRoadAttachment(contentStream))
+            {
+                await writer.WriteAttachmentAsBase64Async(attachment);
+                await outputStream.FlushAsync();
+            }
+
+            outputStream.Position = 0;
+            var encodedOutput = await new StreamReader(outputStream).ReadToEndAsync();
+
+            var lines = encodedOutput.Split(new [] { "\r\n" }, StringSplitOptions.None);
+            Assert.Equal(6, lines.Length);
+            Assert.Equal("TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz", lines[0]);
+            Assert.Equal("IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg", lines[1]);
+            Assert.Equal("dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu", lines[2]);
+            Assert.Equal("dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo", lines[3]);
+            Assert.Equal("ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=", lines[4]);
+            Assert.Equal("", lines[5]);
+
+            var encoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
+
+            Assert.NotEqual(encoded, encodedOutput);
+            Assert.Equal(encoded, encodedOutput.Replace("\r\n", ""));
+        }
     }
 }
